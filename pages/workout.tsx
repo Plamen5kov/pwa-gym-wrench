efaec05cd4fc4267a4e3905a29337e5648636350
plamen petkov <plamen.krasimirov.petkov@gmail.com>
2024-01-22 09:23:04 +0200

    work on accordeon

--- pages/workout.tsx
+++ pages/workout.tsx
@@ -1,29 +1,94 @@
+import AccordeonMy from '@/components/accordeon';
+import Accordeon from '@/components/accordeon';
 import Page from '@/components/page'
 import Section from '@/components/section'
 
-const Story = () => (
-	<Page>
-		<Section>
-			<h2 className='text-xl font-semibold'>Story</h2>
+import { Accordion } from "flowbite";
+import type { AccordionOptions, AccordionItem, AccordionInterface } from "flowbite";
+import type { InstanceOptions } from 'flowbite';
+import { useEffect } from 'react';
 
-			<div className='mt-2'>
-				<p className='text-zinc-600 dark:text-zinc-400'>
-					&quot;I confess that when this all started, you were like a picture
-					out of focus to me. And it took time for my eyes to adjust to you, to
-					make sense of you, to really recognize you.&quot;
-				</p>
+const Story = () => {
 
-				<br />
+	useEffect(() => {
+		// const accordionEl = document.querySelector('#accordion-example') as HTMLElement;
 
-				<p className='text-sm text-zinc-600 dark:text-zinc-400'>
-					<a href='https://twosentencestories.com/vision' className='underline'>
-						Vision
-					</a>
-					, a two sentence story
-				</p>
-			</div>
-		</Section>
-	</Page>
-)
+		// create an array of objects with the id, trigger element (eg. button), and the content element
+		// const accordionItems: AccordionItem[] = [
+		// 	{
+		// 		id: 'accordion-example-heading-1',
+		// 		triggerEl: document.querySelector('#accordion-example-heading-1') as HTMLElement,
+		// 		targetEl: document.querySelector('#accordion-example-body-1') as HTMLElement,
+		// 		active: false
+		// 	},
+		// 	{
+		// 		id: 'accordion-example-heading-2',
+		// 		triggerEl: document.querySelector('#accordion-example-heading-2') as HTMLElement,
+		// 		targetEl: document.querySelector('#accordion-example-body-2') as HTMLElement,
+		// 		active: false
+		// 	},
+		// 	{
+		// 		id: 'accordion-example-heading-3',
+		// 		triggerEl: document.querySelector('#accordion-example-heading-3') as HTMLElement,
+		// 		targetEl: document.querySelector('#accordion-example-body-3') as HTMLElement,
+		// 		active: false
+		// 	}
+		// ];
+
+		// // options with default values
+		// const options: AccordionOptions = {
+		// 	alwaysOpen: true,
+		// 	activeClasses: 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white',
+		// 	inactiveClasses: 'text-gray-500 dark:text-gray-400',
+		// 	onOpen: (item) => {
+		// 		console.log('accordion item has been shown');
+		// 		console.log(item);
+		// 	},
+		// 	onClose: (item) => {
+		// 		console.log('accordion item has been hidden');
+		// 		console.log(item);
+		// 	},
+		// 	onToggle: (item) => {
+		// 		console.log('accordion item has been toggled');
+		// 		console.log(item);
+		// 	},
+		// };
+
+		// instance options object
+		const instanceOptions: InstanceOptions = {
+			id: 'accordion-example',
+			override: true
+		};
+
+		/*
+		* accordionEl: HTML element (required)
+		* accordionItems: array of accordion item objects (required)
+		* options (optional)
+		* instanceOptions (optional)
+		*/
+		// const accordion: AccordionInterface = new Accordion(accordionEl, accordionItems, options, instanceOptions);
+
+		// open accordion item based on id
+		// accordion.open('accordion-example-heading-2');
+
+		// destroy accordion
+		// accordion.destroy();
+
+		// re-initialize accordion
+	})
+
+	return (
+		<Page>
+			<Section>
+				<h2 className='text-xl font-semibold'>Workout</h2>
+
+				<div></div>
+
+				<AccordeonMy  />
+
+			</Section>
+		</Page>
+	)
+}
 
 export default Story
