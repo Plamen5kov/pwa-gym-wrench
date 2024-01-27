5d7db82e513a85be4fa42dd346920620645f2d93
plamen petkov <plamen.krasimirov.petkov@gmail.com>
2024-01-27 13:14:42 +0200

    add body visualizer

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 const links = [
-	{ label: 'Story', href: '/story' },
-	{ label: 'Recipes', href: '/recipes' },
+	{ label: 'Workout', href: '/workout' },
+	{ label: 'Measurements', href: '/measurements' },
 ]
 
 const Appbar = () => {
--- pages/workout.tsx
+++ pages/workout.tsx
@@ -1,90 +1,47 @@
 import AccordeonMy from '@/components/accordeon';
-import Accordeon from '@/components/accordeon';
 import Page from '@/components/page'
 import Section from '@/components/section'
-
-import { Accordion } from "flowbite";
-import type { AccordionOptions, AccordionItem, AccordionInterface } from "flowbite";
-import type { InstanceOptions } from 'flowbite';
-import { useEffect } from 'react';
+import React from 'react';
+import Model, { IExerciseData, IMuscleStats } from 'react-body-highlighter';
 
 const Story = () => {
 
-	useEffect(() => {
-		// const accordionEl = document.querySelector('#accordion-example') as HTMLElement;
+	const data: IExerciseData[] = [
+		{ name: 'Bench Press', muscles: ['chest', 'triceps', 'front-deltoids'], frequency: 1 },
+		{ name: 'Push Ups', muscles: ['chest'], frequency: 2 },
+	];
 
-		// create an array of objects with the id, trigger element (eg. button), and the content element
-		// const accordionItems: AccordionItem[] = [
-		// 	{
-		// 		id: 'accordion-example-heading-1',
-		// 		triggerEl: document.querySelector('#accordion-example-heading-1') as HTMLElement,
-		// 		targetEl: document.querySelector('#accordion-example-body-1') as HTMLElement,
-		// 		active: false
-		// 	},
-		// 	{
-		// 		id: 'accordion-example-heading-2',
-		// 		triggerEl: document.querySelector('#accordion-example-heading-2') as HTMLElement,
-		// 		targetEl: document.querySelector('#accordion-example-body-2') as HTMLElement,
-		// 		active: false
-		// 	},
-		// 	{
-		// 		id: 'accordion-example-heading-3',
-		// 		triggerEl: document.querySelector('#accordion-example-heading-3') as HTMLElement,
-		// 		targetEl: document.querySelector('#accordion-example-body-3') as HTMLElement,
-		// 		active: false
-		// 	}
-		// ];
+	const handleClick = React.useCallback(({ muscle, data }: IMuscleStats) => {
+		const { exercises, frequency } = data;
 
-		// // options with default values
-		// const options: AccordionOptions = {
-		// 	alwaysOpen: true,
-		// 	activeClasses: 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white',
-		// 	inactiveClasses: 'text-gray-500 dark:text-gray-400',
-		// 	onOpen: (item) => {
-		// 		console.log('accordion item has been shown');
-		// 		console.log(item);
-		// 	},
-		// 	onClose: (item) => {
-		// 		console.log('accordion item has been hidden');
-		// 		console.log(item);
-		// 	},
-		// 	onToggle: (item) => {
-		// 		console.log('accordion item has been toggled');
-		// 		console.log(item);
-		// 	},
-		// };
+		alert(`You clicked the ${muscle}! You've worked out this muscle ${frequency} times through the following exercises: ${JSON.stringify(exercises)}`)
 
-		// instance options object
-		const instanceOptions: InstanceOptions = {
-			id: 'accordion-example',
-			override: true
-		};
-
-		/*
-		* accordionEl: HTML element (required)
-		* accordionItems: array of accordion item objects (required)
-		* options (optional)
-		* instanceOptions (optional)
-		*/
-		// const accordion: AccordionInterface = new Accordion(accordionEl, accordionItems, options, instanceOptions);
-
-		// open accordion item based on id
-		// accordion.open('accordion-example-heading-2');
-
-		// destroy accordion
-		// accordion.destroy();
-
-		// re-initialize accordion
-	})
+	}, [data]);
 
 	return (
 		<Page>
+			<Section>
+				<div className='grid grid-cols-2 gap-15'>
+					<Model
+						data={data}
+						style={{ width: '15rem'  }}
+						onClick={handleClick}
+					/>
+					<Model
+						data={data}
+						style={{ width: '15rem' }}
+						onClick={handleClick}
+						type='posterior'
+					/>
+				</div>
+
+			</Section>
 			<Section>
 				<h2 className='text-xl font-semibold'>Workout</h2>
 
 				<div></div>
 
-				<AccordeonMy  />
+				<AccordeonMy />
 
 			</Section>
 		</Page>
